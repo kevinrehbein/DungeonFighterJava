dddcc0e30beac51333d5bbe2c7fafa6171c7967a
Kevin Rehbein <kevin2.rehbein@gmail.com>
2024-08-15 23:53:42 -0300

    Classe personagem

--- DungeonFighterJava/src/dungeonfighterjava/DungeonFighterJava.java
+++ DungeonFighterJava/src/dungeonfighterjava/DungeonFighterJava.java
@@ -1,9 +1,20 @@
 package dungeonfighterjava;
 
+import javax.swing.*;
+
 public class DungeonFighterJava {
 
     public static void main(String[] args) {
         
+        JFrame mainScreen = new JFrame("Welcome");
+        mainScreen.setVisible(true);
+        mainScreen.setSize(500, 300);
+        mainScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        mainScreen.setLayout(null);
+        JButton button1 = new JButton("Jogar");
+        button1.setBounds(150, 125, 200, 50);
+        mainScreen.add(button1);
     }
     
 }
